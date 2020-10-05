817369ff56cd70b174d725176829a9d113751f59
Godfrey Necesario <53319117+pengpa0012@users.noreply.github.com>
2020-10-05 16:40:32 +0800

    updated portfolio layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,29 @@
+
+
+/* Nav Toggle */
+
+const navList = document.querySelector('.nav-list')
 const navBurger = document.querySelector('.nav-burger')
-const navContent = document.querySelector('.nav-content')
-
-
-navBurger.addEventListener('click', navToggle)
-
-function navToggle(){
-	navContent.classList.toggle('toggle')
-}
-
-
-const openModalBtn = document.querySelectorAll('[data-modal-target]')
-const closeModalBtn = document.querySelectorAll('[data-close-btn]')
+const navBurgerLine = document.querySelectorAll('.nav-burger .line')
 const overlay = document.querySelector('.overlay')
 
-console.log(openModalBtn, closeModalBtn)
+navBurger.addEventListener('click', ()=>{
 
+	navList.classList.toggle('active')
+	overlay.classList.toggle('active')
 
-openModalBtn.forEach(btn =>{
-	btn.addEventListener('click', ()=>{
-		const modal = document.querySelector(btn.dataset.modalTarget)
-
-		openModal(modal)
+	navBurgerLine.forEach(line =>{
+		line.classList.toggle('active')
 	})
+	
+
 })
 
-closeModalBtn.forEach(btn =>{
-	btn.addEventListener('click', () =>{
-		const modal = btn.closest('.modal')
-		closeModal(modal)
-	})
-})
 
 overlay.addEventListener('click', ()=>{
-	const modals = document.querySelectorAll('.modal')
-
-	modals.forEach(modal => {
-		closeModal(modal)
+	navList.classList.toggle('active')
+	overlay.classList.toggle('active')
+	navBurgerLine.forEach(line =>{
+		line.classList.toggle('active')
 	})
 })
-
-
-function openModal(modal){
-	if(modal == null) return
-
-	modal.classList.add('active')
-	overlay.classList.add('active')
-}
-
-function closeModal(modal){
-	if(modal == null) return
-	
-	modal.classList.remove('active')
-	overlay.classList.remove('active')
-}
-
