d44b7c5d422a3ad14b7e717f7ebdd46e6330f614
Godfrey Necesario <53319117+pengpa0012@users.noreply.github.com>
2020-10-08 05:20:31 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,102 @@ overlay.addEventListener('click', ()=>{
 		line.classList.toggle('active')
 	})
 })
+
+// Svg Animation
+
+const dot = document.querySelectorAll('.dot')
+
+const colorArray = [
+	'#173F5F',
+	'#20639B',
+	'#3CAEA3',
+	'#F6D55C',
+	'#ED553B'
+]
+
+dot.forEach(el => {
+	el.addEventListener('click', ()=>{
+		const randomNum = Math.floor(Math.random() * 5)
+	
+		el.style.animation = 'none'
+		el.style.fill = colorArray[randomNum]
+
+
+		console.log(colorArray[randomNum])
+
+	})
+})
+
+
+
+// Text Animation
+
+const heading = document.querySelector('.heading')
+const headingSplit = heading.textContent.split("")
+
+heading.textContent = ''
+
+headingSplit.forEach(head => {
+
+	heading.innerHTML += `<span class="head-text">${ head }</span>`
+
+})
+
+
+let charIndex = 0
+let timer = setInterval(onTick, 50)
+
+function onTick(){
+
+	const span = heading.querySelectorAll('span')[charIndex]
+	span.classList.add('fade')
+	charIndex++
+	if(charIndex === heading.children.length){
+		completed()
+		return
+	}
+}
+
+function completed(){
+	clearInterval(timer)
+	timer = null
+}
+
+// Banner Animation
+
+window.addEventListener('load', ()=>{
+
+	const subHeading = document.querySelector('.sub-heading')
+
+	subHeading.classList.add('fade')
+
+})
+
+// Sections Animation on Scroll
+
+window.addEventListener('scroll', ()=>{
+
+	const allLeft = document.querySelectorAll('.left')
+	let screenPos = window.innerHeight / 1.5
+
+	allLeft.forEach(left => {
+
+		let leftPos = left.getBoundingClientRect().top
+
+		if(leftPos < screenPos) left.classList.add('active')
+		if(leftPos > screenPos) left.classList.remove('active')	
+
+	})
+
+	const allRight = document.querySelectorAll('.right')
+
+	allRight.forEach(right => {
+
+		let rightPos = right.getBoundingClientRect().top
+
+		if(rightPos < screenPos) right.classList.add('active')
+		if(rightPos > screenPos) right.classList.remove('active')	
+
+	})
+
+})
