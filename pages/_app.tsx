e2d5acd7f992bfd35b08120a4f3246079569818f
Pengpa0012 <peng.ilikha@gmail.com>
2022-09-09 21:30:04 +0800

    add other pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,34 @@ import type { AppProps } from 'next/app'
 import Navbar from '../src/components/Navbar'
 import Footer from '../src/components/Footer'
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    const cursor = document.querySelector<HTMLElement>(".cursor")!
+    const navLinks = document.querySelectorAll(".nav-link, .cursor-hover")
+
+    document.addEventListener("mousemove", (e) => {
+      let mousex = e.clientX
+      let mousey = e.clientY
+
+      cursor.style.top = `${mousey}px`
+      cursor.style.left = `${mousex}px`
+    })
+
+    navLinks.forEach(link => {
+      link.addEventListener("mouseenter", () => {
+        cursor.classList.add("hover")
+      })
+
+      link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("hover")
+      })
+    })
+
+  }, [])
+  
   return (
     <>
     <Head>
