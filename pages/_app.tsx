e981be7b36575561510fd65b56c7e2bbde08b690
Pengpa0012 <peng.ilikha@gmail.com>
2022-09-09 21:00:11 +0800

    update nav, add footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../src/components/Navbar'
 import Footer from '../src/components/Footer'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+      <script src="https://cdn.tailwindcss.com"></script>
+    </Head>
     <Navbar />
     <Component {...pageProps} />
     <Footer />
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,16 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 function Navbar() {
+  const router = useRouter()
   return (
-    <div>Navbar</div>
+    <div className="fixed inset-x-0 container flex justify-between items-center px-4 py-6 nav-bar">
+      <h1 className="text-2xl nav-link" onClick={() => router.push("/")}>Logo</h1>
+      <ul className="flex">
+        <li className="mr-8 nav-link" onClick={() => router.push("/works")}>WORKS</li>
+        <li className="nav-link" onClick={() => router.push("/about")}>ABOUT</li>
+      </ul>
+    </div>
   )
 }
 
