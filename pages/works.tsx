2c0bdc8db8083ea0c27c76a466959bd385f7725b
Pengpa0012 <peng.ilikha@gmail.com>
2022-09-12 20:06:26 +0800

    update work card, add about content

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,48 +1,47 @@
-import React, { ClassicComponentClass, useEffect } from 'react'
+import React, { ClassicComponentClass, useEffect, useState } from 'react'
 import WorkCard from '../src/components/WorkCard'
+import { workDetails } from "../src/contents/workContents"
 
 const Works = () => {
+  const [tab, setTab] = useState(0)
 
   useEffect(() => {
     const cursor = document.querySelector<HTMLElement>(".cursor")!
     const workCard = document.querySelectorAll(".work-card")
-    hover(workCard, cursor, "card")
-  }, [])
 
-  const hover = (arrayClass: NodeListOf<Element>, targetClass: Element, type: string) => {
-    arrayClass.forEach(card => {
+    workCard.forEach(card => {
       card.addEventListener("mouseenter", () => {
-        targetClass.classList[type == "card" ? "add" : "remove"]("change-blend")
+        cursor.classList.add("change-blend")
       })
       card.addEventListener("mouseleave", () => {
-        targetClass.classList[type == "card" ? "remove" : "add"]("change-blend")
+        cursor.classList.remove("change-blend")
       })
     })
-  }
+
+  }, [tab])
   
   return (
     <div className="container py-40 text-black-custom">
       <div className="flex justify-between items-center">
         <h1 className="text-3xl">LIST OF MY WORKS</h1>
-        <div className="flex">
-          <p className="mr-6">View by</p>
+        <div className="flex items-center">
+          <p className="mr-4">View by</p>
           <ul className="flex items-center list-icons">
-            <li className="mr-4">
-              <svg viewBox="0 0 32 32" height="32" width="32" style={{ fill: "none" }}><g data-name="Layer 2" id="Layer_2"><path fill="#8d32e3" d="M26,26H6a3,3,0,0,1-3-3V9A3,3,0,0,1,6,6H26a3,3,0,0,1,3,3V23A3,3,0,0,1,26,26ZM6,8A1,1,0,0,0,5,9V23a1,1,0,0,0,1,1H26a1,1,0,0,0,1-1V9a1,1,0,0,0-1-1Z"/><path fill="#8d32e3" d="M14,22H8a1,1,0,0,1,0-2h6a1,1,0,0,1,0,2Z"/><path fill="#8d32e3" d="M28,13H4a1,1,0,0,1,0-2H28a1,1,0,0,1,0,2Z"/></g><g id="frame"><rect className="cls-1" height="32" width="32"/></g></svg>
+            <li className={`mr-2 ${tab == 0 ? "active" : ""}`} onClick={() => setTab(0)}>
+              <svg viewBox="0 0 32 32" height="32" width="32" style={{ fill: "none" }}><g data-name="Layer 2" id="Layer_2"><path fill="#8d32e3" className="fill" d="M26,26H6a3,3,0,0,1-3-3V9A3,3,0,0,1,6,6H26a3,3,0,0,1,3,3V23A3,3,0,0,1,26,26ZM6,8A1,1,0,0,0,5,9V23a1,1,0,0,0,1,1H26a1,1,0,0,0,1-1V9a1,1,0,0,0-1-1Z"/><path fill="#8d32e3" className="fill" d="M14,22H8a1,1,0,0,1,0-2h6a1,1,0,0,1,0,2Z"/><path fill="#8d32e3" className="fill" d="M28,13H4a1,1,0,0,1,0-2H28a1,1,0,0,1,0,2Z"/></g><g id="frame"><rect className="cls-1" height="32" width="32"/></g></svg>
             </li>
-            <li>
-              <svg height="48" viewBox="0 0 48 48" width="48" fill="#8d32e3"><path d="M6 26h4v-4h-4v4zm0 8h4v-4h-4v4zm0-16h4v-4h-4v4zm8 8h28v-4h-28v4zm0 8h28v-4h-28v4zm0-20v4h28v-4h-28z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>
+            <li className={tab == 1 ? "active" : ""} onClick={() => setTab(1)}>
+              <svg height="48" viewBox="0 0 48 48" width="48" fill="#8d32e3" className="fill"><path d="M6 26h4v-4h-4v4zm0 8h4v-4h-4v4zm0-16h4v-4h-4v4zm8 8h28v-4h-28v4zm0 8h28v-4h-28v4zm0-20v4h28v-4h-28z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>
             </li>
           </ul>
         </div>
       </div>
-      <div className="flex flex-wrap justify-center my-12 gap-4 sm:gap-24 px-4">
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
+      <div className={`flex flex-wrap justify-center my-12 gap-4 ${tab == 0 ? "sm:gap-24" : "sm:gap-4"} px-4`}>
+        {
+          workDetails.map((work, i) => (
+            <WorkCard details={work} card={tab == 0 ? true : false} key={`card-${i}`} />
+          ))  
+        }
       </div>
     </div>
   )
--- src/components/WorkCard/index.tsx
+++ src/components/WorkCard/index.tsx
@@ -1,28 +1,52 @@
 import React from 'react'
 
-function WorkCard() {
+interface Card {
+  card: boolean
+  details: {
+    image: string
+    title: string
+    description: string
+    tags: string[]
+  }
+}
+
+function WorkCard({details, card}: Card) {
   return (
-    <div className="shadow-md bg-white max-w-2xl text-black-custom rounded-lg work-card flex flex-col sm:flex-row overflow-hidden">
-      <img src="https://via.placeholder.com/268" className="max-w-lg" draggable="false" />
-      <div className="p-6 flex flex-col justify-between">
-        <div>
-          <h1 className="text-xl">CARD</h1>
-          <p className="text-md leading-snug">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis id nobis provident beatae. Officia quidem accusantium amet consequatur praesentium repellat.</p>
-          <div className="mt-1 flex">
-            <span className="text-sm mr-2">TAGS:</span>
-            <ul className="flex text-sm">
-              <li className="mx-1">TAG</li>
-              <li className="mx-1">TAG</li>
-              <li className="mx-1">TAG</li>
-            </ul>
+    <>
+      {
+        card ? 
+        <div className="shadow-md bg-white max-w-2xl text-black-custom rounded-lg work-card flex flex-col sm:flex-row overflow-hidden">
+          <img src={details.image} className="max-w-lg" draggable="false" />
+          <div className="p-6 flex flex-col justify-between">
+            <div>
+              <h1 className="text-xl">{details.title}</h1>
+              <p className="text-md leading-snug">{details.description}</p>
+              <div className="mt-1 flex">
+                <span className="text-sm mr-2">TAGS:</span>
+                <ul className="flex text-sm">
+                  {
+                    details.tags.map((tag, i) => (
+                      <li className="mx-1" key={`tag-${i}`}>{tag}</li>
+                    ))
+                  }
+                </ul>
+              </div>
+            </div>
+            <div className="flex mt-4">
+              <button className="py-2 px-6 rounded-md mr-2 btn-primary">DEMO</button>
+              <button className="py-2 px-6 rounded-md btn-primary-secondary">CODE</button>
+            </div>
           </div>
         </div>
-        <div className="flex mt-4">
-          <button className="py-2 px-6 rounded-md mr-2 btn-primary">DEMO</button>
-          <button className="py-2 px-6 rounded-md btn-primary-secondary">CODE</button>
+      : 
+      <div className="shadow-md bg-white w-full text-black-custom rounded-lg work-card overflow-hidden">
+        <div className="flex items-center justify-between p-6">
+          <h1 className="text-xl">{details.title}</h1>
+          <h1>ICON</h1>
         </div>
       </div>
-    </div>
+      }
+    </>
   )
 }
 
