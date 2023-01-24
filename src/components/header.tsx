a9cfdf9cc991a22136d93597559c7ca38c5b83e4
Pengpa0012 <peng.ilikha@gmail.com>
2023-01-24 08:57:36 +0800

    added darkmode toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,31 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
+import { component$, useStore, useStyles$ } from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
 
-export default component$(() => {
+interface headerProps {
+  store: {
+    toggle: boolean;
+  }
+}
 
+export default component$((props: headerProps) => {
   const { pathname } = useLocation();
 
   return (
-    <header class="container flex justify-between py-6">
-      <h1 class="text-xl">LOGO</h1>
-      <nav> 
-        <ul class="flex">
-          <li class="text-xl cursor-pointer">Projects</li>
-          <li class="ml-4 text-xl cursor-pointer">About</li>
-          <li class="ml-4 text-xl cursor-pointer">Toggle</li>
-        </ul>
-      </nav>
+    <header class={`fixed inset-x-0 border border-t-0 border-r-0 border-l-0 duration-200 ${props.store.toggle ? "bg-gray-700" : "bg-white"}`}>
+      <div class="container flex justify-between py-6">
+        <h1 class="text-xl">LOGO</h1>
+        <nav> 
+          <ul class="flex">
+            <li class="text-xl cursor-pointer">Projects</li>
+            <li class="ml-4 text-xl cursor-pointer">About</li>
+            <li class="ml-4 text-xl cursor-pointer border rounded-full w-12 grid items-center px-1" onClick$={() => props.store.toggle = !props.store.toggle}>
+              <div class="bg-gray-500 w-4 h-4 rounded-full duration-200" style={{ transform: props.store.toggle ? "translateX(140%)" : "" }}></div>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 });
+// Add toggler icon
+// Add darkmode classes
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,15 +1,17 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useStore } from '@builder.io/qwik';
 import Footer from '~/components/footer';
 import Header from '~/components/header';
 
 export default component$(() => {
+  const store = useStore({toggle: false})
+
   return (
-    <>
-      <Header />
+    <div class={`duration-200 ${store.toggle ? "text-white bg-gray-800" : "text-black bg-white"}`}>
+      <Header store={store} />
       <main>
         <Slot />
       </main>
       <Footer />
-    </>
+    </div>
   );
 });
