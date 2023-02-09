351640d4380771cf69b329c3d30e0c53a5ce183a
Pengpa0012 <peng.ilikha@gmail.com>
2023-02-10 05:05:05 +0800

    removed footer, header

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,4 @@
 import { component$, Slot, useStore } from '@builder.io/qwik';
-import Footer from '~/components/footer';
-import Header from '~/components/header';
 
 export default component$(() => {
   const store = useStore({toggle: false})
