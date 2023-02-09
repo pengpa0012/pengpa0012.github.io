3de30fe033d13434edd293e82345e1dde2b66b25
Pengpa0012 <peng.ilikha@gmail.com>
2023-02-10 05:08:50 +0800

    removed store

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,6 @@
-import { component$, Slot, useStore } from '@builder.io/qwik';
+import { component$, Slot } from '@builder.io/qwik';
 
 export default component$(() => {
-  const store = useStore({toggle: false})
 
   return (
     <div>
