49d8c254559a209bd65950bb5fe5b58e3dde7411
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-02 20:28:26 +0800

    hide project

--- src/Content/index.js
+++ src/Content/index.js
@@ -15,22 +15,22 @@ export const images = [
 ]
 
 export const projectsContent = [
-  {
-    title: "T3 Blog App",
-    image_url: "/images/blog-app.png",
-    description: "Simple blog app using T3 Stack that allows users to create and share their own posts while fostering engagement through commenting. With the ability to create and delete posts.",
-    icons: [],
-    demo_link: "https://t3-blog-app-nine.vercel.app/",
-    code_link: "https://github.com/pengpa0012/t3-blog-app"
-  },
-  {
-    title: "SOCIAL APP",
-    image_url: "/images/social-app.png",
-    description: "I developed a dynamic social media app using MERN stack that allows users to easily signup and login to create and delete posts. Additionally,  users can follow and unfollow other users, and interact with their posts by commenting and liking.",
-    icons: [],
-    demo_link: "https://mern-social-app-client.vercel.app/",
-    code_link: "https://github.com/pengpa0012/mern-social-app"
-  },
+  // {
+  //   title: "T3 Blog App",
+  //   image_url: "/images/blog-app.png",
+  //   description: "Simple blog app using T3 Stack that allows users to create and share their own posts while fostering engagement through commenting. With the ability to create and delete posts.",
+  //   icons: [],
+  //   demo_link: "https://t3-blog-app-nine.vercel.app/",
+  //   code_link: "https://github.com/pengpa0012/t3-blog-app"
+  // },
+  // {
+  //   title: "SOCIAL APP",
+  //   image_url: "/images/social-app.png",
+  //   description: "I developed a dynamic social media app using MERN stack that allows users to easily signup and login to create and delete posts. Additionally,  users can follow and unfollow other users, and interact with their posts by commenting and liking.",
+  //   icons: [],
+  //   demo_link: "https://mern-social-app-client.vercel.app/",
+  //   code_link: "https://github.com/pengpa0012/mern-social-app"
+  // },
   // {
   //   title: "PRODUCT MANAGEMENT",
   //   image_url: "/images/product-management.png",
